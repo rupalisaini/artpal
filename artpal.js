2e89e7dc494868bb5d143d2b5b064e2fb74e62f3
rupali saini <rupalisni1999@gmail.com>
2019-08-09 01:37:28 +0530

    Added inner labels for colour codes

--- artpal.js
+++ artpal.js
@@ -2,13 +2,26 @@ function change() {
     var x=document.getElementsByClassName("square");
     var i;
     for(i = 0; i < x.length; i++){
-        x[i].style.backgroundColor = "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")";
+        x[i].style.backgroundColor = "RGB(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")";
+        x[i].innerHTML = x[i].style.backgroundColor;
     }
 }
 
 function squareChange(x){
     var x = document.getElementById(x);
-    x.style.backgroundColor = "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")";
+    var r, g, b;
+    r = (Math.floor(Math.random() * 256));
+    g = (Math.floor(Math.random() * 256));
+    b = (Math.floor(Math.random() * 256));
+    x.style.backgroundColor = "RGB(" + r + "," + g + "," + b + ")";
+}
+
+function toWhite(){
+    document.body.style.backgroundColor = "white";
+}
+
+function toBlack(){
+    document.body.style.backgroundColor = "black";
 }
 
 /*function changeColour(){
