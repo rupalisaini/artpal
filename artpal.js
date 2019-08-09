3067811bf21cf0b80e46fcdfbf762396527b2328
rupali saini <rupalisni1999@gmail.com>
2019-08-09 21:23:20 +0530

    Added borders and background color options

--- artpal.js
+++ artpal.js
@@ -4,6 +4,7 @@ function change() {
     for(i = 0; i < x.length; i++){
         x[i].style.backgroundColor = "RGB(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")";
         x[i].innerHTML = x[i].style.backgroundColor;
+        x[i].style.borderColor = "transparent";
     }
 }
 
@@ -14,14 +15,30 @@ function squareChange(x){
     g = (Math.floor(Math.random() * 256));
     b = (Math.floor(Math.random() * 256));
     x.style.backgroundColor = "RGB(" + r + "," + g + "," + b + ")";
+    x.innerHTML = x.style.backgroundColor;
+    x.style.borderColor = "transparent";
 }
 
 function toWhite(){
     document.body.style.backgroundColor = "white";
+    var x=document.getElementsByClassName("square");
+    var i;
+    for(i = 0; i < x.length; i++){
+        if(x[i].style.borderColor != "black" && x[i].style.borderColor != "transparent"){
+            x[i].style.borderColor = "black";            
+        }
+    }
 }
 
 function toBlack(){
     document.body.style.backgroundColor = "black";
+    var x=document.getElementsByClassName("square");
+    var i;
+    for(i = 0; i < x.length; i++){
+        if(x[i].style.borderColor != "white" && x[i].style.borderColor != "transparent"){
+            x[i].style.borderColor = "white";            
+        }
+    }
 }
 
 /*function changeColour(){
