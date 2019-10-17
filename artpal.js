30b96a22d9b65f7ee2674fba5a8270d17b7bfcf7
rupali saini <rupalisni1999@gmail.com>
2019-10-16 21:43:57 -0400

    put the labels outside the squares for better visibility

--- artpal.js
+++ artpal.js
@@ -3,20 +3,24 @@ function change() {
     var i;
     for(i = 0; i < x.length; i++){
         x[i].style.backgroundColor = "RGB(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")";
-        x[i].innerHTML = x[i].style.backgroundColor;
+        let id = "label" + (i + 1);
+        let label = document.getElementById(id);
+        label.innerHTML = x[i].style.backgroundColor;
         x[i].style.borderColor = "transparent";
     }
 }
 
 function squareChange(x){
-    var x = document.getElementById(x);
+    var square = document.getElementById(x);
+    let id = "label" + x;
+    let label = document.getElementById(id);
     var r, g, b;
     r = (Math.floor(Math.random() * 256));
     g = (Math.floor(Math.random() * 256));
     b = (Math.floor(Math.random() * 256));
-    x.style.backgroundColor = "RGB(" + r + "," + g + "," + b + ")";
-    x.innerHTML = x.style.backgroundColor;
-    x.style.borderColor = "transparent";
+    square.style.backgroundColor = "RGB(" + r + "," + g + "," + b + ")";
+    label.innerHTML = square.style.backgroundColor;
+    square.style.borderColor = "transparent";
 }
 
 function toWhite(){
@@ -25,8 +29,11 @@ function toWhite(){
     var i;
     for(i = 0; i < x.length; i++){
         if(x[i].style.borderColor != "black" && x[i].style.borderColor != "transparent"){
-            x[i].style.borderColor = "black";            
+            x[i].style.borderColor = "black";
         }
+        let id = "label" + (i + 1);
+        let label = document.getElementById(id);
+        label.style.color = "black";
     }
 }
 
@@ -36,8 +43,11 @@ function toBlack(){
     var i;
     for(i = 0; i < x.length; i++){
         if(x[i].style.borderColor != "white" && x[i].style.borderColor != "transparent"){
-            x[i].style.borderColor = "white";            
+            x[i].style.borderColor = "white";
         }
+        let id = "label" + (i + 1);
+        let label = document.getElementById(id);
+        label.style.color = "white";
     }
 }
 
