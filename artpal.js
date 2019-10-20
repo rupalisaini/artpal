2073bdea27ca3ee985d23bb60a00670e13e9e973
rupali saini <rupalisni1999@gmail.com>
2019-10-19 21:02:24 -0400

    cleaned up js

--- artpal.js
+++ artpal.js
@@ -50,8 +50,3 @@ function toBlack(){
         label.style.color = "white";
     }
 }
-
-/*function changeColour(){
-    document.getElementById("sq").style.backgroundColor = "rgb(" + (Math.floor(Math.random() * 20) + 230) + "," + (Math.floor(Math.random() * 20) + 230) + "," + (Math.floor(Math.random() * 20) + 230) + ")"
-}
-setInterval(changeColour, 1000);*/
